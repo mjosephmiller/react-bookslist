ed29ab265ef69e8c59c242564e00e7ec8d92c1ef
Mikey <mjosephmiller.uk@gmail.com>
2016-10-13 16:06:29 +0100

    booklist appears on page

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Harry Potter & The Prisoner of Azkaban'},
+    {title: 'Harry Potter & The Chamber of Secrets'},
+    {title: 'Harry Potter & The Half Blood Prince'},
+    {title: "Harry Potter & The Philosopher's Stone"}
+  ]
+}
