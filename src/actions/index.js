a550429c04e5d56ac24c24a7f7a20a0f2024176f
Mikey <mjosephmiller.uk@gmail.com>
2016-10-13 17:57:28 +0100

    has a book detail div

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book){
-  console.log('A book has been selected:', book.title)
+//selectBook is an Action Creator, it needs to return an action,
+//an object with a property called type
+  return{
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
